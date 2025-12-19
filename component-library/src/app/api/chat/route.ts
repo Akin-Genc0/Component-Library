10329e763135a316cfcd9fc198fd1d55f9ce8bce
Akin-Genc0 <akingenc212@gmail.com>
2025-12-19 10:09:30 +0000

    Add linting workflow and format code with Prettier

--- component-library/src/app/api/chat/route.ts
+++ component-library/src/app/api/chat/route.ts
@@ -1,48 +1,46 @@
-import { OpenAI } from "openai"
-import { NextRequest, NextResponse } from 'next/server'
+import { OpenAI } from "openai";
+import { NextRequest, NextResponse } from "next/server";
 import { InferenceClient } from "@huggingface/inference";
 
-
-   export async function POST(request: NextRequest) {
-    const client = new OpenAI({
-      baseURL: "https://router.huggingface.co/v1",
-      apiKey: process.env.NEXT_PUBLIC_HF_TOKEN as string,
-      dangerouslyAllowBrowser: true,
-    });
-
-  const data = await request.json()
-  const message = data.message
-
-if(message.includes("generate image")){
-  const client = new InferenceClient(process.env.NEXT_PUBLIC_HF_TOKEN as string);
-
-const image = await client.textToImage({
-    provider: "wavespeed",
-    model: "black-forest-labs/FLUX.1-dev",
-    inputs: message,
-    parameters: { num_inference_steps: 5, width: 512, height: 512 },
-});
-
-  return NextResponse.json({ 
-    message: `data:image/png;base64,${image}`
+export async function POST(request: NextRequest) {
+  const client = new OpenAI({
+    baseURL: "https://router.huggingface.co/v1",
+    apiKey: process.env.NEXT_PUBLIC_HF_TOKEN as string,
+    dangerouslyAllowBrowser: true,
   });
 
+  const data = await request.json();
+  const message = data.message;
 
-}
+  if (message.includes("generate image")) {
+    const client = new InferenceClient(
+      process.env.NEXT_PUBLIC_HF_TOKEN as string
+    );
 
-const chatCompletion = await client.chat.completions.create({
- 
-      model: "openai/gpt-oss-120b:together",
-     
-      messages: [
-        {
-          role: "user",
-          content: message,
-        },
-      ],
+    const image = await client.textToImage({
+      provider: "wavespeed",
+      model: "black-forest-labs/FLUX.1-dev",
+      inputs: message,
+      parameters: { num_inference_steps: 5, width: 512, height: 512 },
     });
-  
-     return NextResponse.json({ 
-      message: chatCompletion.choices[0].message.content 
+
+    return NextResponse.json({
+      message: `data:image/png;base64,${image}`,
     });
   }
+
+  const chatCompletion = await client.chat.completions.create({
+    model: "openai/gpt-oss-120b:together",
+
+    messages: [
+      {
+        role: "user",
+        content: message,
+      },
+    ],
+  });
+
+  return NextResponse.json({
+    message: chatCompletion.choices[0].message.content,
+  });
+}
--- component-library/src/lib/actions/login.ts
+++ component-library/src/lib/actions/login.ts
@@ -1,11 +1,11 @@
-"use server"
+"use server";
 import { signIn } from "@/authConfig";
 import { signOut } from "@/authConfig";
 
 export const login = async () => {
-    await signIn('github', {redirectTo: "/"})
-}
+  await signIn("github", { redirectTo: "/" });
+};
 
 export const logout = async () => {
-    await signOut({redirectTo: "/"})
-}
+  await signOut({ redirectTo: "/" });
+};
