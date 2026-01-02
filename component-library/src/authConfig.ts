84082c0d0ac52b0915756babdfd7b36cc21ff23d
Akin-Genc0 <akingenc212@gmail.com>
2026-01-02 13:32:11 +0000

    feat: setup Prisma with Neon database and NextAuth integration

--- component-library/src/authConfig.ts
+++ component-library/src/authConfig.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./db/client";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 });
