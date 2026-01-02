db91cdf0446086058c453dea8b40c09a918c223b
Akin-Genc0 <akingenc212@gmail.com>
2026-01-02 14:45:54 +0000

    ci: add Google OAuth credentials to CI/CD workflow and add google provider to nextauth

--- component-library/src/authConfig.ts
+++ component-library/src/authConfig.ts
@@ -1,9 +1,28 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./db/client";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub],
+    providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!
+    }),
+    GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+  })
+  ]
 });
+
+ 
--- component-library/src/lib/actions/login.ts
+++ component-library/src/lib/actions/login.ts
@@ -2,8 +2,12 @@
 import { signIn } from "@/authConfig";
 import { signOut } from "@/authConfig";
 
-export const login = async () => {
+export const loginGithub = async () => {
   await signIn("github", { redirectTo: "/" });
+};
+
+export const loginGoogle = async () => {
+  await signIn("google", { redirectTo: "/" });
 };
 
 export const logout = async () => {
